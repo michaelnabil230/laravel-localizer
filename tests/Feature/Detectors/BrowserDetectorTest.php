6b6c4676d7d5bbad243b33e361fb8ab0f18bd8f2
Adam Nielsen <info@adam-nielsen.de>
2026-04-27 13:13:27 +0200

    test: add tests for BrowserDetector

---
+++ tests/Feature/Detectors/BrowserDetectorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace NielsNumbers\LocaleRouting\Tests\Feature\Detectors;
+
+use Illuminate\Http\Request;
+use NielsNumbers\LocaleRouting\Detectors\BrowserDetector;
+use Orchestra\Testbench\TestCase;
+
+class BrowserDetectorTest extends TestCase
+{
+    /** @test */
+    public function it_returns_null_when_no_accept_language_header_is_present()
+    {
+        $request = Request::create('/');
+        $request->headers->remove('Accept-Language');
+
+        $this->assertNull((new BrowserDetector())->detect($request));
+    }
+
+    /** @test */
+    public function it_returns_ordered_locale_candidates_from_accept_language()
+    {
+        $request = Request::create('/', 'GET', [], [], [], [
+            'HTTP_ACCEPT_LANGUAGE' => 'de-DE,de;q=0.9,en;q=0.8',
+        ]);
+
+        $result = (new BrowserDetector())->detect($request);
+
+        $this->assertIsArray($result);
+        $this->assertSame(['de-DE', 'de', 'en'], $result);
+    }
+
+    /** @test */
+    public function it_orders_by_quality_weight()
+    {
+        $request = Request::create('/', 'GET', [], [], [], [
+            'HTTP_ACCEPT_LANGUAGE' => 'fr;q=0.5,en;q=0.9,de;q=0.8',
+        ]);
+
+        $result = (new BrowserDetector())->detect($request);
+
+        $this->assertSame(['en', 'de', 'fr'], $result);
+    }
+}
